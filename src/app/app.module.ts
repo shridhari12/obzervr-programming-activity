72b97a9a1225ad6277c29daf29896490c95c6bbb
shridhariyer12@gmail.com <shridhariyer12@gmail.com>
2020-06-24 07:37:37 +1000

    initial project structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+//import { MessagesComponent } from './components/messages/messages.component';
+
+@NgModule({
+  declarations: [
+    AppComponent//,
+    //MessagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
